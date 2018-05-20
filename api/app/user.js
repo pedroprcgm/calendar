0406732b7f0d71793e85f9d5cf552a08961ebd27
Pedro Guerreiro <pedro.prcgm@gmail.com>
2018-05-20 05:48:36 +0100

    New Features

--- api/app/user.js
+++ api/app/user.js
@@ -1,7 +1,73 @@
+const chai = require('chai'),
+    assert = chai.assert,
+    { comparePassword } = require('../infra/encryption'),
+    jwt = require('../infra/jwt');
 
 const user = {};
 
-module.exports = (app) => {    
-
-    return user;    
+const _addUser = (model, user) => {
+    return new Promise((resolve, reject) => {
+        model.create(user)
+            .then(added => {
+                resolve(added)
+            }, err => {
+                console.log(err); // TODO: implement error log
+                reject(err);
+            });
+    });
 };
+
+const _validateFields = user => {
+
+    // assert.isNotEmpty(user.name);
+    // assert.isNotEmpty(user.email);
+    // assert.isNotEmpty(user.password);    
+
+    return true;
+};
+
+const _validatePassword = (password, encrypted) => {
+    return comparePassword(password, encrypted);
+};
+
+user.add = (models, user) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const _valid = _validateFields(user);
+            if (!_valid) {
+                throw Error({ code: 400, err: 'Validation problems', msg: 'Validation problems. Check the fields.' });
+            }
+
+            _addUser(models.user, user)
+                .then(
+                    success => resolve(success),
+                    err => { throw Error({ code: 500, err: err }) }); // TODO: implement error log
+
+        } catch (err) {
+            reject(err);
+        }
+    });
+};
+
+user.validate = (models, user) => {
+    return new Promise((resolve, reject) => {
+        try {
+            models.user.findOne({ where: { email: user.email } })
+                .then(userData => {
+
+                    if (!_validatePassword(user.password, userData.password)) {
+                        throw Error({ code: 400, err: 'Invalid credentials', msg: 'Invalid credentials' });
+                    }
+
+                    resolve(userData);
+
+                }, err => {
+                    throw Error({ code: 500, err: '', msg: '' }); // TODO: implement error log 
+                });
+        } catch (error) {
+            reject(error);
+        }
+    });
+};
+
+module.exports = user;
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,15 +1,28 @@
 const { Router } = require('express'),
-    jwt = require('../infra/jwt');
-
+    jwt = require('../infra/jwt'),
+    user = require('../app/user');
 const router = Router();
 
 router.post('/', (req, res, next) => {
-    jwt.signIn(req.body)
-    .then( success => {
-        res.send(success);
-    }, err => {
-        res.sendStatus(500).send(err);
-    })
+    user.validate(req.models, req.body)
+        .then(user => {
+            jwt.signIn({ email: user.email, password: user.password })
+                .then(token => res.send({token: token}),
+                    err => {
+                        if (err.code === 400) res.boom.badRequest(err.msg);
+                        else res.boom.badImplementation(err.err);
+                    });
+        });
+});
+
+router.post('/create', (req, res, next) => {
+    user.add(req.models, req.body)
+        .then(success => {
+            res.send(success);
+        }, err => {
+            if (err.code === 400) res.boom.badRequest(err.msg);
+            else res.boom.badImplementation(err.err);
+        });
 });
 
 
