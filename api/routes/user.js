45a7977011ece77ffe78af464816cfcea0f44b14
Pedro Guerreiro <pedro.prcgm@gmail.com>
2018-05-21 00:41:04 +0100

    New feature

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,12 +2,26 @@ const { Router } = require('express'),
     router = Router(),
     user = require('../app/user');
 
-router.get('/', (req, res, next) => {    
+router.get('/', (req, res, next) => {
     res.send('Hello World');
 });
 
-router.post('/', (req, res, next) => {
-    user
+router.put('/:id', (req, res, next) => {
+    user.update(req.models, req.body, req.params.id)
+        .then( user => res.send(user))
+        .catch( err => res.boom.badImplementation(err.msg))
+});
+
+router.delete('/:id', (req, res, next) => {
+    user.delete(req.models, req.params.id)
+        .then( () => res.sendStatus(204))
+        .catch( err => {
+            if (err.code === 400) res.boom.badRequest(err.msg);
+            else res.boom.badImplementation(err.err);
+        });
+});
+
+router.post('/', (req, res, next) => {    
 });
 
 module.exports = router;
