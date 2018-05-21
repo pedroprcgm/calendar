9adbc3aa0a68b2f8bd45100e7d383ec10be18f0d
Pedro Guerreiro <pedro.prcgm@gmail.com>
2018-05-21 02:32:54 +0100

    Add decode token middleware

--- api/middlewares/jwt.js
+++ api/middlewares/jwt.js
@@ -2,11 +2,15 @@ const jwt = require('../infra/jwt');
 
 module.exports = {
     checkToken: (req, res, next) => {
-        jwt.verify(req.headers['Authorization'] || req.headers['authorization'])
-        .then(success => {
-            next();
-        }, err => {
-            res.sendStatus(401);
-        })
+        const token = req.headers['Authorization'] || req.headers['authorization'];
+        jwt.verify(token)
+            .then( decode => {
+                req.decode = decode;
+                next();
+            })
+            .catch( err => {
+                console.log(err)
+                res.boom.unauthorized();
+            })
     }    
 };
