34aa1806e47f4b50d124b31edb42e404f35b9f77
Pedro Guerreiro <pedro.prcgm@gmail.com>
2018-05-21 02:34:18 +0100

    New features
    # add event 
    # edit event

---
+++ api/app/event.js
@@ -0,0 +1,72 @@
+const { comparePassword } = require('../infra/encryption'),
+    jwt = require('../infra/jwt'),
+    errorHandler = require('../infra/util/error-handler');
+
+const event = {};
+
+const _addEvent = (model, event) => {
+    return new Promise((resolve, reject) => {
+        model.create(event)
+            .then(added => {
+                resolve(added)
+            })
+            .catch(err => {
+                reject(err);
+            });
+    });
+};
+
+const _validateFields = event => {
+
+    // TODO: validation  
+
+    return true;
+};
+
+const _getEvent = async (model, id) => {
+    const event = await model.findOne({ where: { id: id } })
+        .then(event => {
+            return event;
+        })
+        .catch(err => { throw Error(err) });
+    return event;
+};
+
+event.add = (models, event) => {
+    return new Promise((resolve, reject) => {
+        const _valid = _validateFields(event);
+        if (!_valid) {
+            reject(errorHandler.badRequest(err));
+        }
+
+        _addEvent(models.event, event)
+            .then(success => resolve(success))
+            .catch(err => { reject(errorHandler.internalServerError(err)) })
+    });
+};
+
+event.update = (models, event, id, userId) => {
+    return new Promise( async (resolve, reject) => {
+
+        const eventData = await _getEvent(models.event, id);
+
+        if(eventData.authorId !== userId) reject(errorHandler.forbidden());
+
+        models.event.update(event, { where: { id: id } })
+            .then(async result => {
+                const event = await _getEvent(models.event, id);
+                resolve(event);
+            })
+            .catch(err => reject(errorHandler.internalServerError(err)));
+    });
+};
+
+event.delete = (models, id) => {
+    return new Promise((resolve, reject) => {
+        models.event.update({ isDeleted: true }, { where: { id: id } })
+            .then(resolve())
+            .catch(err => reject(err))
+    });
+};
+
+module.exports = event;
--- api/routes/event.js
+++ api/routes/event.js
@@ -2,6 +2,11 @@ const { Router } = require('express'),
     router = Router(),
     event = require('../app/event');
 
+
+const _validateFields = (event) => {
+    
+};
+
 /**
  * @param {Object} Body req.body
  * @desc Create an event
@@ -10,6 +15,17 @@ const { Router } = require('express'),
  */
 router.post('/', (req, res, next) => {
 
+    const userId = req.decode.id;
+    const eventData = Object.assign({}, req.body, {authorId: userId});
+
+    event.add(req.models, eventData)
+        .then(success => {
+            res.send(success);
+        })
+        .catch(err => {
+            if (err.code === 400) res.boom.badRequest(err.msg);
+            else res.boom.badImplementation(err.err);
+        });
 });
 
 /**
@@ -20,7 +36,12 @@ router.post('/', (req, res, next) => {
  * @return Error - Status 500
  */
 router.put('/:id', (req, res, next) => {
-
+    event.update(req.models, req.body, req.params.id, req.decode.id)
+        .then( event => res.send(event))
+        .catch( err => { 
+            if (err.code === 403) res.boom.forbidden(err.msg);
+            else res.boom.badImplementation(err.err);
+        })
 });
 
 /** ### TODO -> events by user
